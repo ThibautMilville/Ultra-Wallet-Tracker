b1ff5c6679279b5ff4f31a761153df6f51fd80db
Thybow <tmilville.pro@gmail.com>
2024-11-29 13:14:01 +0100

    Style update

--- src/App.tsx
+++ src/App.tsx
@@ -7,21 +7,25 @@ const queryClient = new QueryClient();
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <div className="min-h-screen bg-[#13151D] flex flex-col items-center justify-center p-4 gap-6">
-        <WalletCounter />
-        <UOSPrice />
-        <span className="absolute bottom-4 text-gray-400 text-sm font-medium tracking-wider">
-          Made by{' '}
-          <a
-            href="https://discord.gg/kyBHXFmG"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="hover:text-[#8C5AE8] transition-colors duration-200"
-          >
-            Ultra Times
-          </a>
-        </span>
-
+      <div className="min-h-screen bg-[#13151D] flex flex-col items-center justify-between p-4">
+        <div className="w-full flex-1 flex flex-col items-center justify-center gap-6 my-4">
+          <WalletCounter />
+          <UOSPrice />
+        </div>
+        
+        <footer className="w-full flex justify-center py-4">
+          <span className="text-gray-400 text-sm font-medium tracking-wider">
+            Made by{' '}
+            <a
+              href="https://discord.gg/kyBHXFmG"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:text-[#8C5AE8] transition-colors duration-200"
+            >
+              Ultra Times
+            </a>
+          </span>
+        </footer>
       </div>
     </QueryClientProvider>
   );
