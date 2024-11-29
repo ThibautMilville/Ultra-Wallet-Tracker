766daa430d6523a9be57440af3af42327bc1696a
Thybow <tmilville.pro@gmail.com>
2024-11-29 12:43:52 +0100

    Add "Made by Ultra Times"

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,18 @@ function App() {
       <div className="min-h-screen bg-[#13151D] flex flex-col items-center justify-center p-4 gap-6">
         <WalletCounter />
         <UOSPrice />
+        <span className="absolute bottom-4 text-gray-400 text-sm font-medium tracking-wider">
+          Made by{' '}
+          <a
+            href="https://discord.gg/kyBHXFmG"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-[#8C5AE8] transition-colors duration-200"
+          >
+            Ultra Times
+          </a>
+        </span>
+
       </div>
     </QueryClientProvider>
   );
